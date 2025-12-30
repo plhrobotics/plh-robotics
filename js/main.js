ce77f470ca825f6da40ca8c5be3568fc29dbdd16
plhrobotics <plhrobotics@gmail.com>
2025-12-30 17:59:11 -0600

    otro cambio

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 console.log("PLH Robotics listo 🚀");
+const verMasBtn = document.getElementById("verMasBtn");
+const proyectosOcultos = document.querySelectorAll(".proyecto.oculto");
+
+verMasBtn.addEventListener("click", () => {
+  proyectosOcultos.forEach(proyecto => {
+    proyecto.style.display = "block";
+  });
+
+  verMasBtn.style.display = "none"; // ocultar botón después
+});
